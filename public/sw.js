7335634e5581e1a67dddc642fdc4c36e953e0003
shu1007 <shuu.na1007@icloud.com>
2021-11-12 14:12:46 +0900

    chrome-extensionのrequestをcacheしないように除外

--- public/sw.js
+++ public/sw.js
@@ -7,19 +7,24 @@ self.addEventListener("activate", (event) => {
 });
 
 const CACHE_NAME = "v1";
+const validateRequest = (request) => {
+    return !request.url.startsWith("chrome-extension://");
+};
 self.addEventListener("fetch", (event) => {
+    if (!validateRequest(event.request)) return;
+
     event.respondWith(
         caches.open(CACHE_NAME).then((cache) => {
             return cache.match(event.request).then((response) => {
                 console.log(
-                    `cache ${JSON.stringify(event.request)} is ${
+                    `cache ${JSON.stringify(event.request.url)} is ${
                         response ? "match" : "not match"
                     }`
                 );
                 return (
                     response ||
                     fetch(event.request).then((response) => {
-                        console.log(`fetch ${event.request}`);
+                        console.log(`fetch ${event.request.url}`);
                         cache.put(event.request, response.clone());
                         return response;
                     })
